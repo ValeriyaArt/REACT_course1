f1fb57c21c488cf7798bea446c68e6b11f8bfc0c
ValeriyaArt <lerartlera@gmail.com>
2021-05-06 11:39:56 +0300

    91 - chrome extensions для react и redux - React JS

--- my-app/src/redux/redux-store.js
+++ my-app/src/redux/redux-store.js
@@ -1,7 +1,7 @@
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-import {applyMiddleware, combineReducers} from "redux";
+import {applyMiddleware, combineReducers, compose} from "redux";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk"
@@ -20,8 +20,9 @@ let reducers = combineReducers({
     form: formReducer,
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store
+window.__store__ = store
 
 export default store

0f43dc606b63ce3565b811db8b9a01d9771f0dca
ValeriyaArt <lerartlera@gmail.com>
2021-05-07 14:55:46 +0300

    93 - paginator, постраничный вывод - Практика ReactJS

--- my-app/src/redux/users-reducer.js
+++ my-app/src/redux/users-reducer.js
@@ -12,7 +12,7 @@ const FOLLOWING_IN_PROGRESS = 'FOLLOWING_IN_PROGRESS';
 
 let initialState = {
     users: [],
-    pageSize: 5,
+    pageSize: 10,
     totalUsersCount: 0,
     currentPage: 1,
     isFetching: false,

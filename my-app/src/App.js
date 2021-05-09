9e6c29e54b289bd13cc1ddaec47f4d1e6451e8f6
ValeriyaArt <lerartlera@gmail.com>
2021-05-09 18:42:49 +0300

    React.lazy и React.Suspense, зачем? #94 - ReactJS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,7 @@ import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route} from 'react-router-dom'
 import {withRouter} from "react-router";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/login/Login";
 import React, {Component} from "react";
@@ -14,6 +12,8 @@ import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
 
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
 
 class App extends Component {
     componentDidMount() {
@@ -29,16 +29,23 @@ class App extends Component {
                     <HeaderContainer/>
                     <Navbar/>
                     <div className='app-wrapper-content'>
-                        <Route path='/dialogs' render={
-                            () =>
-                                <DialogsContainer/>
-                        }
-
-                        />
-                        <Route path='/profile/:userId?' render={() =>
-                            <ProfileContainer/>}/>
-                        <Route path='/users' render={() =>
-                            <UsersContainer/>}/>
+                        <Route path='/dialogs'
+                               render={() => {
+                                return <React.Suspense fallback={<Preloader/>}>
+                                    <DialogsContainer/>
+                                </React.Suspense>
+                            }
+                        }/>
+                        <Route path='/profile/:userId?' render={() => {
+                            return <React.Suspense fallback={<Preloader/>}>
+                                <ProfileContainer/>
+                            </React.Suspense>
+                        }}/>
+                        <Route path='/users' render={() => {
+                            return <React.Suspense fallback={<Preloader/>}>
+                                <UsersContainer/>
+                            </React.Suspense>
+                        }}/>
                         <Route path='/login' render={() =>
                             <Login/>}/>
 

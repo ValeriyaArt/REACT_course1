e886b595d2886402df8f916a538f7c8c29abcd4f
ValeriyaArt <lerartlera@gmail.com>
2021-05-11 00:50:06 +0300

    Updates

---
+++ precache-manifest.dab89265339c446e5f707a4977b30b07.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c2e95e1a6d3ee1bd85",
+    "url": "/REACT_course1/static/js/0.e443d0c6.chunk.js"
+  },
+  {
+    "revision": "129bee13224ecab0107f",
+    "url": "/REACT_course1/static/css/main.c072ed7e.chunk.css"
+  },
+  {
+    "revision": "129bee13224ecab0107f",
+    "url": "/REACT_course1/static/js/main.8a27ea4b.chunk.js"
+  },
+  {
+    "revision": "6ed41f088f9d26459885",
+    "url": "/REACT_course1/static/js/runtime~main.f4180970.js"
+  },
+  {
+    "revision": "8479b35f14e11cde0abe",
+    "url": "/REACT_course1/static/js/3.a95fb6f7.chunk.js"
+  },
+  {
+    "revision": "e87cea13efae87be05cf",
+    "url": "/REACT_course1/static/css/4.b7299cb2.chunk.css"
+  },
+  {
+    "revision": "e87cea13efae87be05cf",
+    "url": "/REACT_course1/static/js/4.6ba738a2.chunk.js"
+  },
+  {
+    "revision": "b921d17b4f668ebb96f6",
+    "url": "/REACT_course1/static/css/5.cfc6fef4.chunk.css"
+  },
+  {
+    "revision": "b921d17b4f668ebb96f6",
+    "url": "/REACT_course1/static/js/5.7f6368f3.chunk.js"
+  },
+  {
+    "revision": "d2f47a62ff02d10e3547",
+    "url": "/REACT_course1/static/css/6.29840c33.chunk.css"
+  },
+  {
+    "revision": "d2f47a62ff02d10e3547",
+    "url": "/REACT_course1/static/js/6.e2fb0fbb.chunk.js"
+  },
+  {
+    "revision": "2bd0fa89b39410b66ddd",
+    "url": "/REACT_course1/static/css/7.7475c5f0.chunk.css"
+  },
+  {
+    "revision": "2bd0fa89b39410b66ddd",
+    "url": "/REACT_course1/static/js/7.04bf0c06.chunk.js"
+  },
+  {
+    "revision": "d5e325d9b3b5e5c9f23d96a0299066c8",
+    "url": "/REACT_course1/static/media/Gear.d5e325d9.gif"
+  },
+  {
+    "revision": "08e6ccaf214ba8d9a7f4f9d0294df585",
+    "url": "/REACT_course1/static/media/user.08e6ccaf.png"
+  },
+  {
+    "revision": "0800f1833b8ece6a48e94fd99eab17b1",
+    "url": "/REACT_course1/index.html"
+  }
+];
